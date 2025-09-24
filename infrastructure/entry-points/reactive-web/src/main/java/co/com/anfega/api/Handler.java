5a6f082fd1916f1f1a60a19d1b129f03c94c6876
Andres Felipe Gañan Moreno <andres.ganan@pragma.com.co>
2025-09-24 17:30:14 -0500

    ADD: helpers

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/anfega/api/Handler.java
@@ -0,0 +1,13 @@
+package co.com.anfega.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class Handler {
+
+}
